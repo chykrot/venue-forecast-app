29f47f39033bfa9f5a536db054904c40b72c0628
Kris <krisdev.garcia@mail.com>
2020-12-18 09:25:23 +0800

    install vue and tailwind

---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      
+        <h1>{{ message }}</h1>
+
+    </div>
+</template>
+<script>
+
+export default {
+    computed: {},
+    data() {
+        return {
+            message:'Test'
+        }
+    }
+};
+</script>
